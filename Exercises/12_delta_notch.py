fb5202eccdb7bac76206ceabd26772a4b27c2ada
daggermaster3000 <quillan.favey@unil.ch>
2022-12-06 13:40:57 +0100

    adfsc

--- Exercises/12_delta_notch.py
+++ Exercises/12_delta_notch.py
@@ -16,7 +16,7 @@ def ddpdt(Np,Dp,b,h):
     return ddpdt
 
 #initial parms for cell 1
-D1 = 0.99
+D1 = 0.5
 N1 = 1
 #cell 2
 D2 = 1
@@ -26,17 +26,20 @@ a = 0.01
 b = 100
 k = 2
 h = 2
+time = 50
+unit = 24*60
+timestep = 1000
 
 Cell1 = {"delta":[D1], "notch":[N1]}
 Cell2 = {"delta":[D2], "notch":[N2]}
 
-for i in range(0,50):
+for i in np.arange(0,time*unit,timestep):
     #for cell 1
-    D1_next = ddpdt(N1,D1,b,h)
-    N1_next = dndt(D2,a,N1,k)
+    D1_next = D1 + ddpdt(N1,D1,b,h)/unit
+    N1_next = N1 + dndt(D2,a,N1,k)/unit
     #for cell 2
-    D2_next = ddpdt(N2,D2,b,h)
-    N2_next = dndt(D1,a,N2,k)
+    D2_next = D2 + ddpdt(N2,D2,b,h)/unit
+    N2_next = N2 + dndt(D1,a,N2,k)/unit
     #update the values
     D1 = D1_next
     D2 = D2_next
@@ -49,11 +52,17 @@ for i in range(0,50):
     Cell2["notch"].append(N2)
 
    
-time = np.arange(0,51)
-print(Cell1["delta"][-1])
+time1 = np.arange(0,time*unit+1,timestep)
+print(f"Delta final cell 1: {Cell1['delta'][-1]} \n notch final cell 1: {Cell1['notch'][-1]}")
+print(f"Delta final cell 2: {Cell2['delta'][-1]} \n notch final cell 2: {Cell2['notch'][-1]}")
 fig,axs = plt.subplots(2)
-axs[0].plot(time,Cell1["delta"])
-axs[0].plot(time,Cell1["notch"])
-axs[1].plot(time,Cell2["delta"])
-axs[1].plot(time,Cell2["notch"])
+fig.suptitle(f"Timestep: {timestep}")
+axs[0].set_title("cell 1")
+axs[0].scatter(time1,Cell1["delta"],label="delta")
+axs[0].plot(time1,Cell1["notch"],label="notch")
+axs[0].legend()
+axs[1].set_title("cell 2")
+axs[1].plot(time1,Cell2["delta"],label="delta")
+axs[1].plot(time1,Cell2["notch"],label="notch")
+axs[1].legend()
     
